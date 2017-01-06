8928d728116460ee527c00f2bc1f5620dd763b8d
arjanzee <arjanzeekant@hotmail.com>
2017-01-06 09:07:24 +0100

    Delete connection.php

--- hosted/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "cl20-secretd-mws", "HDg6Y^7X4", "cl20-secretd-mws");
-        if (mysqli_connect_error()) {
-            die ("Database Connection Error");
-        }
-?>
